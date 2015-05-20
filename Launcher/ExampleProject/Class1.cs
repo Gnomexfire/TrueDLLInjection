7838cdcf334fbca8f4bfb7ae3e281158146f62eb
RafaelFernandesMota <rafaelfernandesmotta@live.com>
2015-05-20 12:29:46 -0300

    Example TrueDLLInjection

---
+++ Launcher/ExampleProject/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ExampleProject 
+{
+    public class Example
+    {
+        public static int EntryPoint(string args)
+        {
+            var proc = Process.GetCurrentProcess().ProcessName;
+            MessageBox.Show(@"Process is : " + proc);
+            if (args != null)
+            {
+                MessageBox.Show(args);
+            }
+            return 0;
+        }
+    }
+}
